4adfcef2358ca2ed973b9a5c3a1f684033e1ff37
Miroslav Douda <miramirinda@outlook.cz>
2022-11-28 11:09:10 +0100

    add typeracer class

--- TypeRacer/TypeRacer/Form1.cs
+++ TypeRacer/TypeRacer/Form1.cs
@@ -4,38 +4,13 @@ namespace TypeRacer
     {
 
         
-        string[] SentencesField;
-        public char[] vetaVCharech;
-        string VybranaVeta;
-        int index = 0;
-
+        
         public Form1()
         {
             InitializeComponent();
             Start();
         }
-        void Start() {
-            NactiSoubor();
-            ZobrazListCharu();
-        }
-        void NactiSoubor() {
-            string sentences = System.IO.File.ReadAllText("vety.txt");
-            SentencesField = sentences.Split('.');
-        }
         
-        void ZobrazListCharu() {
-            //TODO zobrazit na UI vìtu
-            label_Text.Text = VyberNahodnouVetu(SentencesField);
-        }
-
-        string VyberNahodnouVetu(string[] sentencesField)
-        {
-            Random rnd = new Random();
-            VybranaVeta = sentencesField[rnd.Next(0, sentencesField.Length - 1)];
-            return VybranaVeta;
-
-        }
-
         private void Form1_PreviewKeyDown(object sender, PreviewKeyDownEventArgs e)
         {
             MessageBox.Show("Adawd");
@@ -50,36 +25,7 @@ namespace TypeRacer
                 //-
             }
         }
-        int VratIndex(int cislo) {
-            return cislo;
-        }
-        void KontrolaPismene(PreviewKeyDownEventArgs e) {
-            vetaVCharech = VybranaVeta.ToCharArray();
-            //TODO kontrola inputu 
-            // -> spravne 
-            if (label_Text.Text[index] == (char)e.KeyCode)
-            {
-                SpravnePismenko();
-            }
-            // -> spatne
-            else
-            {
-                ChybnePismenko();
-            }
-        }
-        void SpravnePismenko() {
-            //TODO obarvit písmenko zelenì
-
-        }
-        void ChybnePismenko() {
-            //TODO obarvit špatné písmenko
-            
-        }
-
-        private void button_GenerujVetu_Click(object sender, EventArgs e)
-        {
-            ZobrazListCharu();
-        }
+        
 
         private void Form1_KeyDown(object sender, KeyEventArgs e)
         {
---
+++ TypeRacer/TypeRacer/TypeRacer.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TypeRacer
+{
+    internal class TypeRacer
+    {
+        string[] SentencesField;
+        public char[] vetaVCharech;
+        string VybranaVeta;
+        int index = 0;
+
+
+        int VratIndex(int cislo)
+        {
+            return cislo;
+        }
+        void KontrolaPismene(PreviewKeyDownEventArgs e)
+        {
+            vetaVCharech = VybranaVeta.ToCharArray();
+            //TODO kontrola inputu 
+            // -> spravne 
+            if (label_Text.Text[index] == (char)e.KeyCode)
+            {
+                SpravnePismenko();
+            }
+            // -> spatne
+            else
+            {
+                ChybnePismenko();
+            }
+        }
+        void SpravnePismenko()
+        {
+            //TODO obarvit písmenko zeleně
+
+        }
+        void ChybnePismenko()
+        {
+            //TODO obarvit špatné písmenko
+
+        }
+
+        private void button_GenerujVetu_Click(object sender, EventArgs e)
+        {
+            ZobrazListCharu();
+        }
+        void Start()
+        {
+            NactiSoubor();
+            ZobrazListCharu();
+        }
+        void NactiSoubor()
+        {
+            string sentences = System.IO.File.ReadAllText("vety.txt");
+            SentencesField = sentences.Split('.');
+        }
+
+        void ZobrazListCharu()
+        {
+            //TODO zobrazit na UI větu
+            label_Text.Text = VyberNahodnouVetu(SentencesField);
+        }
+
+        string VyberNahodnouVetu(string[] sentencesField)
+        {
+            Random rnd = new Random();
+            VybranaVeta = sentencesField[rnd.Next(0, sentencesField.Length - 1)];
+            return VybranaVeta;
+
+        }
+
+    }
+}
